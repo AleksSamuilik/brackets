79eb9ee3edd2a86bfd4ddc22b202c0667886a4fd
Spawn <alkspawn@gmail.com>
2020-03-10 22:30:49 +0300

    default

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, config) {
+  const pairs = config.map(element => element.join(""));
+  let string = str,
+   length;
+  while (length !== string.length){
+      length = string.length;
+      pairs.forEach(e => {
+          string = string.replace(e, "");
+      });
+  };
+
+  return string.length === 0;
 }
